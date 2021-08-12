af4ce073c7a1dad917e2cd9c9c73f4470559da3c
cihan.dogan <cihan.dogan@asseco-see.com.tr>
2021-08-12 23:48:26 +0300

    application Test edited

---
+++ src/test/java/com/cihan/kalah/KalahApplicationTest.java
@@ -0,0 +1,11 @@
+package com.cihan.kalah;
+
+import org.junit.jupiter.api.Test;
+
+class KalahApplicationTest {
+
+    @Test
+    void main() {
+        KalahApplication.main(new String[]{});
+    }
+}
